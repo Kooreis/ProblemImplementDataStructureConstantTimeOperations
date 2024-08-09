19552ef8e6bad5456ac3bc94fb368362878b92f4
Kooreis <kooreis.official@gmail.com>
2024-08-08 22:42:01 -0500

    This update adds the 'inc' method to the AllOne class. This method increments the count of a key, updates the counts data structure, and adjusts the maxCount and minCount variables as necessary.

--- solution.py
+++ solution.py
@@ -1,9 +1,9 @@
-import collections
-
-class AllOne:
-    def __init__(self):
-        self.keys = collections.defaultdict(int)
-        self.counts = collections.defaultdict(collections.deque)
-        self.maxCount = 0
-        self.minCount = float('inf')
-        self.counts[0] = collections.deque([''])
+def inc(self, key: str) -> None:
+        self.keys[key] += 1
+        self.counts[self.keys[key]].append(key)
+        self.counts[self.keys[key] - 1].remove(key)
+        if not self.counts[self.keys[key] - 1]:
+            del self.counts[self.keys[key] - 1]
+        self.maxCount = max(self.maxCount, self.keys[key])
+        if self.keys[key] - 1 == self.minCount and not self.counts[self.keys[key] - 1]:
+            self.minCount += 1
